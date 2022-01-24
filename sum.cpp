f742f83a86e4cb146a5cee2b74c6ee8dac1915ab
Aashish Sharma <96761521+aashisharma22@users.noreply.github.com>
2022-01-25 01:19:49 +0530

    Sum of two numbers
    Adding two numbers by taking input from the user.

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, sum;
+    cout<<"Enter Two Numbers: ";
+    cin>>num1>>num2;
+    sum = num1+num2;
+    cout<<"\nResult = "<<add;
+    cout<<endl;
+    return 0;
+}
