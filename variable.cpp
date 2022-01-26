61a8b94bdafb67882f3caf4087f261941483672a
Aashish Sharma <96761521+aashisharma22@users.noreply.github.com>
2022-01-27 01:09:33 +0530

    Taking input from user

---
+++ variable.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int  age;
+    char name;
+    
+
+	cout << "Enter your name:";
+	cin >> name;
+	cout << "\nYour name is: " << name;
+
+    cout << "Enter your age:";
+	cin >> age;
+	cout << "\nYour age is: " << age;
+
+	return 0;
+}
